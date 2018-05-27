faeee47ef4b55dde8c00001e3c4b0d76dfef8048
Haseeb Akbar <haseeb2000@hotmail.com>
2018-05-27 18:33:11 +0100

    Added rocketship sounds to when it flies.

--- Space Cuisine/Assets/Scripts/Rocket.cs
+++ Space Cuisine/Assets/Scripts/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,7 +21,13 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space)) rigidBody.AddRelativeForce(Vector3.up); // can rotate when thrusting. Use relative so acts on direction.
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up); // can rotate when thrusting. Use relative so acts on direction.
+            if(!audioSource.isPlaying) audioSource.Play();
+        }
+
+        else audioSource.Stop();
 
         if (Input.GetKey(KeyCode.A)) transform.Rotate(Vector3.forward); // z axis rotate
 
