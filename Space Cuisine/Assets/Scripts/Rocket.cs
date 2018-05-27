6a1a3075a07045c6a7c881628d2ed9b1d54be1c2
Haseeb Akbar <haseeb2000@hotmail.com>
2018-05-27 16:53:58 +0100

    Able to rotate and hover.

--- Space Cuisine/Assets/Scripts/Rocket.cs
+++ Space Cuisine/Assets/Scripts/Rocket.cs
@@ -21,8 +21,8 @@ public class Rocket : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Space)) rigidBody.AddRelativeForce(Vector3.up); // can rotate when thrusting. Use relative so acts on direction.
 
-        if (Input.GetKey(KeyCode.A)) print("Tilt to the left");
+        if (Input.GetKey(KeyCode.A)) transform.Rotate(Vector3.forward); // z axis rotate
 
-        else if (Input.GetKey(KeyCode.D)) print("Tilt to the right");
+        else if (Input.GetKey(KeyCode.D)) transform.Rotate(-Vector3.forward);
     }
 }
