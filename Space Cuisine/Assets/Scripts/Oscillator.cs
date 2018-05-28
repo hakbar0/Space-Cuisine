a8d8838ab2b2ed1fe73311e9744d30d4c51ab823
Haseeb Akbar <haseeb2000@hotmail.com>
2018-05-28 19:10:30 +0100

    Have a moving platform.

--- Space Cuisine/Assets/Scripts/Oscillator.cs
+++ Space Cuisine/Assets/Scripts/Oscillator.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour {
 
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f; // defualt at 2 seconds
 
     Vector3 startingPos;
-
-    [Range(0,1)]
-    [SerializeField]
     float movementFactor;
 
 	// Use this for initialization
@@ -20,6 +18,14 @@ public class Oscillator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
+        float cycles = Time.time/ period;
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from minus 1 to plus 1
+        movementFactor = rawSinWave / 2f + 0.5f; // as we want 1f not 0,5;
+
+
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
 	}
